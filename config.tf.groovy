71321737916f36f5e69985b5130a33f1bf3120f6
Alex Ens <ensalex2004@gmail.com>
2024-07-29 15:49:30 +0500

    66

--- config.tf.groovy
+++ config.tf.groovy
@@ -14,20 +14,12 @@ resource "yandex_compute_disk" "boot-disk-1" {
   name     = "boot-disk-1"
   type     = "network-hdd"
   zone     = "ru-central1-b"
-  size     = "20"
-  image_id = "fd88q8cetpv706g8kia9"
-}
-
-resource "yandex_compute_disk" "boot-disk-2" {
-  name     = "boot-disk-2"
-  type     = "network-hdd"
-  zone     = "ru-central1-b"
-  size     = "20"
-  image_id = "fd88q8cetpv706g8kia9"
+  size     = "30"
+  image_id = "fd801iv1gjj2mbvjolim"
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+  name = "test2"
 
   resources {
     cores  = 2
@@ -48,27 +40,6 @@ resource "yandex_compute_instance" "vm-1" {
   }
 }
 
-resource "yandex_compute_instance" "vm-2" {
-  name = "terraform2"
-
-  resources {
-    cores  = 4
-    memory = 4
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk-2.id
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    user-data = "${file("/home/user/yc/meta.txt")}"
-  }
-}
 
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
@@ -85,14 +56,6 @@ output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
 
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-}
-
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
