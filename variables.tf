dff05ac784e1c8e685f72d9e41666c1afaee961b
Alex Ens <ensalex2004@gmail.com>
2024-07-31 11:04:58 +0500

    2

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "service_account_key_file" {
+    description = "Service account key file"
+    type = string
+    default = "/home/user/terraform/dc2507"
+}
+variable "cloud_id" {
+    description = "Cloud ID"
+    type = string
+  
+}
+variable "image_id" {
+    description = "Image ID"
+    type = string
+  
+}
